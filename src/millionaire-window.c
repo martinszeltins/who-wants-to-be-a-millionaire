88e4ea93b63dd46023405ec7d0ff835e2ddb03b0
Martins <martins.zeltins@arkbauer.com>
2022-11-13 11:53:38 +0200

    Added all the answers

--- src/millionaire-window.c
+++ src/millionaire-window.c
@@ -55,7 +55,7 @@ void millionaire_window_gameplay_start(MillionaireWindow * self)
     strcpy(self->questions[0].lifeline_fifty_fifty_hide[0], "A");
     strcpy(self->questions[0].lifeline_fifty_fifty_hide[1], "B");
     strcpy(self->questions[0].lifeline_call_answer, "I know that Hamlet and Romeo are but Darren looks like it could be the one that is not.");
-    strcpy(self->questions[0].lifeline_audience_answer, "The audience has voted:\nA: 0%\nB:0%\nC:40%\nD:60%");
+    strcpy(self->questions[0].lifeline_audience_answer, "The audience has voted:\nA: 2%\nB: 7%\nC: 38%\nD: 53%");
 
     strcpy(self->questions[1].question, "What sort of animal is Walt Disney's Dumbo?");
     strcpy(self->questions[1].answers[0], "Deer");
@@ -67,6 +67,102 @@ void millionaire_window_gameplay_start(MillionaireWindow * self)
     strcpy(self->questions[1].lifeline_fifty_fifty_hide[0], "A");
     strcpy(self->questions[1].lifeline_fifty_fifty_hide[1], "D");
     strcpy(self->questions[1].lifeline_call_answer, "Dumbo was the Elephant, wasn't it?");
-    strcpy(self->questions[1].lifeline_audience_answer, "The audience has voted:\nA: 13%\nB:24%\nC:80%\nD:10%");
+    strcpy(self->questions[1].lifeline_audience_answer, "The audience has voted:\nA: 7%\nB: 3%\nC: 80%\nD: 10%");
+
+    strcpy(self->questions[2].question, "What does the word loquacious mean?");
+    strcpy(self->questions[2].answers[0], "Angry");
+    strcpy(self->questions[2].answers[1], "Chatty");
+    strcpy(self->questions[2].answers[2], "Beautiful");
+    strcpy(self->questions[2].answers[3], "Shy");
+    strcpy(self->questions[2].correct_answer, "B");
+    strcpy(self->questions[2].money_earned_if_answered, "$300,000");
+    strcpy(self->questions[2].lifeline_fifty_fifty_hide[0], "C");
+    strcpy(self->questions[2].lifeline_fifty_fifty_hide[1], "D");
+    strcpy(self->questions[2].lifeline_call_answer, "Hmm, I don't know... But if I had to guess I would say chatty.");
+    strcpy(self->questions[2].lifeline_audience_answer, "The audience has voted:\nA: 3%\nB: 87%\nC: 4%\nD: 6%");
+
+    strcpy(self->questions[3].question, "What does the word loquacious mean?");
+    strcpy(self->questions[3].answers[0], "Angry");
+    strcpy(self->questions[3].answers[1], "Chatty");
+    strcpy(self->questions[3].answers[2], "Beautiful");
+    strcpy(self->questions[3].answers[3], "Shy");
+    strcpy(self->questions[3].correct_answer, "B");
+    strcpy(self->questions[3].money_earned_if_answered, "$400,000");
+    strcpy(self->questions[3].lifeline_fifty_fifty_hide[0], "C");
+    strcpy(self->questions[3].lifeline_fifty_fifty_hide[1], "D");
+    strcpy(self->questions[3].lifeline_call_answer, "Hmm, I don't know... But if I had to guess I would say chatty.");
+    strcpy(self->questions[3].lifeline_audience_answer, "The audience has voted:\nA: 3%\nB: 87%\nC: 4%\nD: 6%");
+
+    strcpy(self->questions[4].question, "Construction of which of these famous landmarks was completed first?");
+    strcpy(self->questions[4].answers[0], "Empire State Building");
+    strcpy(self->questions[4].answers[1], "Royal Albert Hall");
+    strcpy(self->questions[4].answers[2], "Eiffel Tower");
+    strcpy(self->questions[4].answers[3], "Big Ben Clock Tower");
+    strcpy(self->questions[4].correct_answer, "D");
+    strcpy(self->questions[4].money_earned_if_answered, "$500,000");
+    strcpy(self->questions[4].lifeline_fifty_fifty_hide[0], "A");
+    strcpy(self->questions[4].lifeline_fifty_fifty_hide[1], "B");
+    strcpy(self->questions[4].lifeline_call_answer, "To be honest, I really don't know. My wild guess is the Eiffel Tower.");
+    strcpy(self->questions[4].lifeline_audience_answer, "The audience has voted:\nA: 9%\nB: 7%\nC: 24%\nD: 62%");
+
+    strcpy(self->questions[5].question, "What is the Celsius equivalent of 77 degrees Fahrenheit?");
+    strcpy(self->questions[5].answers[0], "15");
+    strcpy(self->questions[5].answers[1], "20");
+    strcpy(self->questions[5].answers[2], "25");
+    strcpy(self->questions[5].answers[3], "30");
+    strcpy(self->questions[5].correct_answer, "C");
+    strcpy(self->questions[5].money_earned_if_answered, "$600,000");
+    strcpy(self->questions[5].lifeline_fifty_fifty_hide[0], "D");
+    strcpy(self->questions[5].lifeline_fifty_fifty_hide[1], "B");
+    strcpy(self->questions[5].lifeline_call_answer, "I think that 100 F is about 30 in celsius. So I will say 15.");
+    strcpy(self->questions[5].lifeline_audience_answer, "The audience has voted:\nA: 11%\nB: 21%\nC: 59%\nD: 9%");
+
+    strcpy(self->questions[6].question, "The word \"aristocracy\" literally means power in the hands of whom?");
+    strcpy(self->questions[6].answers[0], "The few");
+    strcpy(self->questions[6].answers[1], "The best");
+    strcpy(self->questions[6].answers[2], "The barons");
+    strcpy(self->questions[6].answers[3], "The rich");
+    strcpy(self->questions[6].correct_answer, "A");
+    strcpy(self->questions[6].money_earned_if_answered, "$700,000");
+    strcpy(self->questions[6].lifeline_fifty_fifty_hide[0], "B");
+    strcpy(self->questions[6].lifeline_fifty_fifty_hide[1], "C");
+    strcpy(self->questions[6].lifeline_call_answer, "I know it's not the best. I think it's the few or the rich.");
+    strcpy(self->questions[6].lifeline_audience_answer, "The audience has voted:\nA: 85%\nB: 3%\nC: 27%\nD: 16%");
+
+    strcpy(self->questions[7].question, "A sports team playing in its own arena is said to have what type of advantage?");
+    strcpy(self->questions[7].answers[0], "Homecoming");
+    strcpy(self->questions[7].answers[1], "Home depot");
+    strcpy(self->questions[7].answers[2], "Home court");
+    strcpy(self->questions[7].answers[3], "Home sweet home");
+    strcpy(self->questions[7].correct_answer, "C");
+    strcpy(self->questions[7].money_earned_if_answered, "$800,000");
+    strcpy(self->questions[7].lifeline_fifty_fifty_hide[0], "D");
+    strcpy(self->questions[7].lifeline_fifty_fifty_hide[1], "A");
+    strcpy(self->questions[7].lifeline_call_answer, "I'm pretty sure it's home court.");
+    strcpy(self->questions[7].lifeline_audience_answer, "The audience has voted:\nA: 1%\nB: 0%\nC: 99%\nD: 0%");
+
+    strcpy(self->questions[8].question, "In which of these games do players use a paddle to hit the ball?");
+    strcpy(self->questions[8].answers[0], "Ping-Pong");
+    strcpy(self->questions[8].answers[1], "Water polo");
+    strcpy(self->questions[8].answers[2], "Shuffleboard");
+    strcpy(self->questions[8].answers[3], "Football");
+    strcpy(self->questions[8].correct_answer, "A");
+    strcpy(self->questions[8].money_earned_if_answered, "$900,000");
+    strcpy(self->questions[8].lifeline_fifty_fifty_hide[0], "B");
+    strcpy(self->questions[8].lifeline_fifty_fifty_hide[1], "D");
+    strcpy(self->questions[8].lifeline_call_answer, "I don't know what Shuffleboard is but that would be my answer.");
+    strcpy(self->questions[8].lifeline_audience_answer, "The audience has voted:\nA: 90%\nB: 2%\nC: 3%\nD: 5%");
+
+    strcpy(self->questions[9].question, "A dermatologist is best suited to treat which of these medical conditions?");
+    strcpy(self->questions[9].answers[0], "Headache");
+    strcpy(self->questions[9].answers[1], "Cavity");
+    strcpy(self->questions[9].answers[2], "Heart murmur");
+    strcpy(self->questions[9].answers[3], "Skin rash");
+    strcpy(self->questions[9].correct_answer, "D");
+    strcpy(self->questions[9].money_earned_if_answered, "$1,000,000");
+    strcpy(self->questions[9].lifeline_fifty_fifty_hide[0], "C");
+    strcpy(self->questions[9].lifeline_fifty_fifty_hide[1], "A");
+    strcpy(self->questions[9].lifeline_call_answer, "It's definitely a heart murmur. Just kidding, I think derma means skin.");
+    strcpy(self->questions[9].lifeline_audience_answer, "The audience has voted:\nA: 19%\nB: 28%\nC: 14%\nD: 58%");
 }
 
